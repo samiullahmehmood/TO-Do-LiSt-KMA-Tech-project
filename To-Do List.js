cbdb3fb84af22f078fb2c74b4444197d5c9212a3
samiullahmehmood <150831752+samiullahmehmood@users.noreply.github.com>
2023-11-22 07:44:31 -0800

    Add files via upload

--- To-Do List.js
+++ To-Do List.js
@@ -11,14 +11,29 @@ inputBx.addEventListener("keyup", function(event){
 let addItem = (inputBx) => {
     let listItem = document.createElement("li");
     listItem.innerHTML = `${inputBx}<i></i>`;
-    
+        saveData();
+
     listItem.addEventListener("click", function(){
         this.classList.toggle('done');
+        saveData();
     })
 
     listItem.querySelector("i").addEventListener("click",function(){
         listItem.remove(); 
+        saveData();
+        
  })
 
     list.appendChild(listItem);
+
 }
+
+function saveData(){ 
+    localStorage.setItem("data", list.innerHTML);
+}
+function showTask(){
+    list.innerHTML = localStorage.getItem("data");
+}
+
+
+
